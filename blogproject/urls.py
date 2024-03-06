41936d8d4084a24c0015f3fdf9cc223e975020f9
Your Name <you@example.com>
2024-03-06 04:01:02 +0300

    update

--- blogproject/urls.py
+++ blogproject/urls.py
@@ -26,4 +26,4 @@ urlpatterns = [
     # path('ckeditor/', include('ckeditor.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-# urlpatterns +=  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns +=  static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
